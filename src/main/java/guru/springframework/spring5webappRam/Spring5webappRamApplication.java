0c28be5c34b2270319a717150e90a61b3c07d211
ronsahota <ronsahota@hotmail.com>
2020-01-27 20:37:36 -0500

    Initial Commit

---
+++ src/main/java/guru/springframework/spring5webappRam/Spring5webappRamApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webappRam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappRamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappRamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5webappRam/Spring5webappRamApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webappRam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappRamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
