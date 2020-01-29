811009244b3781f2203ade99b4d6ac63ccbe0777
ronsahota <ronsahota@hotmail.com>
2020-01-28 21:35:31 -0500

    Added equals and hash code method along with toString to ensure the author and book sets have unique ID object reference.

--- src/main/java/guru/springframework/spring5webappRam/model/Book.java
+++ src/main/java/guru/springframework/spring5webappRam/model/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webappRam.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -71,5 +72,29 @@ public class Book {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
     }
 }
