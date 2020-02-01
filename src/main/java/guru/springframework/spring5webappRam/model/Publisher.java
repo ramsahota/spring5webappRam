5ed01391f757ede72b1d3201014287b2d7f09f74
ronsahota <ronsahota@hotmail.com>
2020-01-31 21:47:32 -0500

    Displaying list of books using Spring MVC Controller with Thymeleaf view template.

--- src/main/java/guru/springframework/spring5webappRam/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webappRam/model/Publisher.java
@@ -23,6 +23,30 @@ public class Publisher {
         this.address = address;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
